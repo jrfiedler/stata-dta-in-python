340b52dec94276a0fd4d1cd099472ef8d8bd3e12
jrfiedler <jrfiedler@gmail.com>
2014-03-13 16:42:42 -0500

    moved DtaVar* classes into separate file and renamed classes to StataVar*

--- stata_dta/stata_dta.py
+++ stata_dta/stata_dta.py
@@ -12,6 +12,7 @@ import sys
 from .stata_missing import (
     get_missing, MissingValue, MISSING, MISSING_VALS
 )
+from .stata_variable import StataVariable
 
 try:
     from stata import st_format
@@ -48,287 +49,12 @@ TB_FMT_RE = re.compile(r'^%(-)?tb([^:]*)(:(' + date_details + ')*)?$')
 MONTH_ABBREV = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 
                 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
 
+                
 # exception to raise when Dta file is mis-formatted
 class DtaParseError(Exception):
     pass
-
     
-class DtaVarVals():
-    """A class for intermediate values when calculating with data
-    variables within a Dta object. Variables themselves are referenced
-    through an instance of DtaVariable. 
     
-    This class is meant for internal use.
-    
-    Example
-    -------    
-    A user can create or replace a data variable called "target" with
-    
-        dta.target_ = dta.input1_ - 2 * dta.input2_
-    
-    When "dta.input1_" and "dta.input2_" attributes are looked up,
-    instances of DtaVariable are returned (assuming variables "input1"
-    and "input2" exist). The calculation on the right hand side results
-    in an instance of DtaVarVals holding the calculated values. The 
-    assignment statement replaces values if the variable exists, or
-    adds a new variable to the dataset.
-    
-    """
-    def __init__(self, values):
-        self.values = values
-    
-    def __setitem__(self, index, value):
-        self.values[index] = value
-        
-    def __getitem__(self, index):
-        return self.values[index]
-            
-    def __abs__(self):
-        return DtaVarVals([abs(v) for v in self.values])
-        
-    def __add__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v + o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v + other for v in self.values])
-        
-    def __bool__(self):
-        return DtaVarVals([bool(v) for v in self.values])
-        
-    def __eq__(self, other):
-        if isinstance(other, collections.Iterable):
-            values = self.values
-            return DtaVarVals(
-                [values[i] == other[i] for i in range(len(self))]
-            )
-        return DtaVarVals([v == other for v in self.values])
-        
-    def __float__(self):
-        return DtaVarVals([float(v) for v in self.values])
-        
-    def __floordiv__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v // o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v // other for v in self.values])
-        
-    def __ge__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v >= o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v >= other for v in self.values])
-        
-    def __gt__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v > o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v > other for v in self.values])
-        
-    def __iadd__(self, other):
-        if isinstance(other, collections.Iterable):
-            self.values = [v + o for (v,o) in zip(self.values, other)]
-        else:
-            self.values = [v + other for v in self.values]
-        return self
-        
-    def __ifloordiv__(self, other):
-        if isinstance(other, collections.Iterable):
-            self.values = [v // o for (v,o) in zip(self.values, other)]
-        else:
-            self.values = [v // other for v in self.values]
-        return self
-        
-    def __imod__(self, other):
-        if isinstance(other, collections.Iterable):
-            self.values = [v % o for (v,o) in zip(self.values, other)]
-        else:
-            self.values = [v % other for v in self.values]
-        return self
-        
-    def __imul__(self, other):
-        if isinstance(other, collections.Iterable):
-            self.values = [v * o for (v,o) in zip(self.values, other)]
-        else:
-            self.values = [v * other for v in self.values]
-        return self
-        
-    def __int__(self):
-        return DtaVarVals([int(v) for v in self.values])
-        
-    def __ipow__(self, other):
-        if isinstance(other, collections.Iterable):
-            self.values = [v ** o for (v,o) in zip(self.values, other)]
-        else:
-            self.values = [v ** other for v in self.values]
-        return self
-        
-    def __isub__(self, other):
-        if isinstance(other, collections.Iterable):
-            self.values = [v - o for (v,o) in zip(self.values, other)]
-        else:
-            self.values = [v - other for v in self.values]
-        return self
-        
-    def __iter__(self):
-        values = self.values
-        for v in values:
-            yield v
-        
-    def __itruediv__(self, other):
-        if isinstance(other, collections.Iterable):
-            self.values = [v / o for (v,o) in zip(self.values, other)]
-        else:
-            self.values = [v / other for v in self.values]
-        return self
-        
-    def __le__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v <= o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v <= other for v in self.values])
-        
-    def __len__(self):
-        return len(self.values)
-        
-    def __lt__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v < o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v < other for v in self.values])
-        
-    def __mod__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v % o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v % other for v in self.values])
-        
-    def __mul__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v * o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v * other for v in self.values])
-        
-    def __ne__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v != o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v != other for v in self.values])
-        
-    def __neg__(self):
-        return DtaVarVals([-v for v in self.values])
-        
-    def __pos__(self):
-        return self
-        
-    def __pow__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v ** o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v ** other for v in self.values])
-        
-    def __radd__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([o + v for (v,o) in zip(self.values, other)])
-        return DtaVarVals([other + v for v in self.values])
-        
-    def __rfloordiv__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([o // v for (v,o) in zip(self.values, other)])
-        return DtaVarVals([other // v for v in self.values])
-        
-    def __rmod__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([o % v for (v,o) in zip(self.values, other)])
-        return DtaVarVals([other % v for v in self.values])
-        
-    def __rmul__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([o * v for (v,o) in zip(self.values, other)])
-        return DtaVarVals([other * v for v in self.values])
-        
-    def __round__(self, n=None):
-        return DtaVarVals([round(v, n) for v in self.values])
-        
-    def __rpow__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([o ** v for (v,o) in zip(self.values, other)])
-        return DtaVarVals([other ** v for v in self.values])
-        
-    def __rsub__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([o - v for (v,o) in zip(self.values, other)])
-        return DtaVarVals([other - v for v in self.values])
-        
-    def __rtruediv__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([o / v for (v,o) in zip(self.values, other)])
-        return DtaVarVals([other / v for v in self.values])
-        
-    def __sub__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v - o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v - other for v in self.values])
-        
-    def __truediv__(self, other):
-        if isinstance(other, collections.Iterable):
-            return DtaVarVals([v / o for (v,o) in zip(self.values, other)])
-        return DtaVarVals([v / other for v in self.values])
-        
-    
-class DtaVariable(DtaVarVals):
-    """A class for referencing a data variable within a Dta object.
-    Class instances are created when the user accesses a variable with
-    "dta.varname_", where "varname" is the full variable name or an
-    unambiguous abbreviation. Anything property ending with a single
-    underscore is assumed to be a reference to a variable.
-    
-    This class is meant for internal use.
-    
-    """
-
-    def __init__(self, source, name):
-        self.source = source
-        self.name = name
-        
-    @property
-    def values(self):
-        src = self.source
-        get = src.get
-        c = src.index(self.name)
-        return [get(r,c) for r in range(len(src))]
-        
-    def __iter__(self):
-        src = self.source
-        get = src.get
-        c = src.index(self.name)
-        for r in range(len(src)):
-            yield get(r, c)
-        
-    def __setattr__(self, name, value):
-        if name == "values":
-            src = self.source
-            c = src.index(self.name)
-            src[:, c] = value
-        else:
-            self.__dict__[name] = value
-    
-    def __setitem__(self, index, value):
-        src = self.source
-        src[index, src.index(self.name)] = value
-        
-    def __getitem__(self, index):
-        src = self.source
-        get = src.get
-        c = src.index(self.name)
-    
-        if isinstance(index, int):
-            return get(index, c)
-            
-        if isinstance(index, slice):
-            start, stop, step = index.indices(len(src))
-            index = range(start, stop, step)
-        elif not isinstance(index, collections.Iterable):
-            raise TypeError("index must be slice, iterable, or int")
-        
-        return [get(i, c) for i in index]
-        
-    def __len__(self):
-        return len(self.source)
-        
-    def __str__(self):
-        return "variable {} of {}".format(self.name, self.source)
-    
-
 class Dta():
     """A Python parent class for Stata datasets. 
     Sub-classes implement methods for particular versions.
@@ -547,7 +273,7 @@ class Dta():
     def __getattr__(self, name):
         """Provides shortcut to Dta variables by appending "_".
         Raises AttributeError if name does not end with "_".
-        Tries to find variable and return DtaVariable otherwise.
+        Tries to find variable and return StataVariable otherwise.
         """
         if not name.endswith("_"):
             msg = "'{}' object has no attribute '{}'"
@@ -555,7 +281,7 @@ class Dta():
             
         varname = self._find_vars((name[:-1],))[0]
         
-        return DtaVariable(self, varname)
+        return StataVariable(self, varname)
         
     def __setattr__(self, name, value):
         """Provides shortcut to Dta variables by appending "_".
@@ -788,7 +514,7 @@ class Dta():
                 raise ValueError(
                     "no variables found for {}".format(zeros, twos))
             # if getting here, twos != "" and zeros == ""
-            raise ValueError("multiple variables found for {}".format(twos))
+            raise ValueError("multiple variables found for '{}'".format(twos))
             
         if not unique:
             return [m[1] for m in matches]
